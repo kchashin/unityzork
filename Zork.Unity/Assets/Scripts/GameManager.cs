c2d0d7c7c14dd9030f3affc24586508e5f730102
kchashin <33077160+kchashin@users.noreply.github.com>
2019-11-19 19:53:30 -0500

    last minute checks

--- Zork.Unity/Assets/Scripts/GameManager.cs
+++ Zork.Unity/Assets/Scripts/GameManager.cs
@@ -57,8 +57,7 @@ public class GameManager : MonoBehaviour
        if (Game.IsRunning == false)
        {
            #if UNITY_EDITOR
-           // Application.Quit() does not work in the editor so
-           // UnityEditor.EditorApplication.isPlaying need to be set to false to end the game
+           
            UnityEditor.EditorApplication.isPlaying = false;
            #else
         Application.Quit();
